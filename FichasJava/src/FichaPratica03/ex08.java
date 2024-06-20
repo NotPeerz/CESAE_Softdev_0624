c59a3893e3eed0e89c5623b650cb9637e13a0120
Enzo <ramalhoenzo453@gmail.com>
2024-06-20 14:23:56 +0100

    c

--- FichasJava/src/FichaPratica03/ex08.java
+++ FichasJava/src/FichaPratica03/ex08.java
@@ -1,4 +1,23 @@
 package FichaPratica03;
 
+import java.util.Scanner;
+
 public class ex08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numero;
+
+
+        System.out.println("Introduza um numero: ");
+        numero = input.nextInt();
+
+
+
+
+
+
+
+
+    }
 }
