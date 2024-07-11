ae1b8899c8731c341fad5c5a01363ac399a5f219
NotPeerz <ramalhoenzo453@gmail.com>
2024-07-10 21:46:18 -0300

    commit

--- FichasJava/src/PE_ENZO_RAMALHO/login.java
+++ FichasJava/src/PE_ENZO_RAMALHO/login.java
@@ -3,13 +3,20 @@ package PE_ENZO_RAMALHO;
 import java.util.Scanner;
 
 public class login {
+    //boolean para admin ser true or false
     public static boolean abrirMenu(boolean admin) {
+
+        //scanner para o login e password
+
         Scanner scanner = new Scanner(System.in);
+
+        //string login e string password
+        
         String login = "admin";
-        String pass = "456";
+        String password = "456";
 
         if (admin=true) {
-            
+
         }
         return admin;
     }
